7ffb9a70aeb83bbba95d09e2b93ac0187d93219f
vrnranjan22 <vrnranjan22@gmail.com>
2020-08-24 21:54:01 +0530

    connection establish

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+connectDB();
+
 const PORT = process.env.port || 5000;
 
-app.get("/", (req, res) => res.send("APi rinnimg"));
+app.get("/", (req, res) => res.send("API running"));
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
